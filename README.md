ce157c5079a0887ce429b37ddc2a37f44e11d87c
Antonio Greco <agreco@mba.local>
2016-05-05 11:22:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+#express-logger
+
+>An express middleware for application access and error logs
+
+#Getting started
+
+Add the package to your project:
+
+```bash
+$ npm install git+https://github.com/scm/www/express-logger.git
+```
+
+Include it in your app:
+
+```javascript
+var express = require('express'),
+    logger = require('express-logger');
+
+app = express();
+
+// Apply the middleware to your application, ready for logging access and errors for all requests
+app.use(logger);
+```
+
+### Info
+
+The middleware listens to every request, logging to STDOUT and STDERR for access and error logging respectively. It does
+not manipulate any request or response.
---
+++ index.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var fs = require('fs'),
+    morgan = require('morgan'),
+    bunyan = require('bunyan'),
+    stream = require('stream').PassThrough,
+    bunyanMiddleware = require('express-bunyan-logger'),
+    bsyslog = require('bunyan-syslog'),
+    msyslog = require('modern-syslog'),
+    log,
+    loggers = {
+        access: function (config) {
+            var msyslogStrm = new msyslog.Stream(),
+                conf = { format: (config && config.format) || 'combined', opts: { stream: new stream() } };
+
+            conf.opts.stream.pipe(process.stdout);
+            if (config && config.file) {
+                conf.opts.stream.pipe(fs.createWriteStream(config.file, { flags: 'a' }));
+            }
+            conf.opts.stream.pipe(msyslogStrm);
+            return morgan(conf.format, conf.opts);
+        },
+        error: function (config) {
+            var opts = {
+                    name: config && config.name || 'myApp',
+                    streams: [
+                        {
+                            level: 'error',
+                            stream: process.stderr
+                        }, {
+                            level: 'error',
+                            type: 'raw',
+                            stream: bsyslog.createBunyanStream({
+                                type: 'sys',
+                                facility: bsyslog.local0,
+                                host: '127.0.0.1',
+                                port: 514
+                            })
+                        }
+                    ]
+                };
+            if (config && config.file) {
+                opts.streams.push({ 'level': 'error', 'path': config.file });
+            }
+            return bunyanMiddleware.errorLogger(opts);
+        }
+    };
+
+module.exports = function (config) {
+    if (!(log instanceof bunyan)) {
+        log = bunyan.createLogger(config);
+        log.register = function register (logType, opts) { return loggers[logType](opts); };
+        log.create = function create (config) { return bunyan.createLogger(config); };
+    }
+    return log;
+};
